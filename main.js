4f51eca5ffc90010142ff7a2fa345d16b670d54f
visakha123 <84570506+visakha123@users.noreply.github.com>
2021-10-07 19:53:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+var first_name = "Visakha";
+console.log(first_name);
+
+var last_name = "Dhar";
+console.log(last_name)
+
+var full_name = first_name.concat(last_name);
+console.log(full_name)
+
+var length_of_full_name = full_name.length;
+console.log(length_of_full_name);
+
+var capital_name = "VISAKHA";
+var making_lower_case = capital_name.toLowerCase();
+
+var small_name = "visakha";
+var making_upper_case = small_name.toUpperCase();
+
+var square_root = Math.sqrt(12);
+console.log(square_root);
+
+var maximum_number = Math.max(77,0.77);
+
+var minimum_number = Math.min(14,114);
+
